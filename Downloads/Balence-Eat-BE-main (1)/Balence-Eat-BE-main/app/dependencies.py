309ccb3fbfc91d76b543a67bee2f5f2620ecf55d
dongjun291 <tiranojang@gachon.ac.kr>
2025-10-17 16:48:35 +0900

    Delete Downloads/Balence-Eat-BE-main (1)/Balence-Eat-BE-main directory

--- Downloads/Balence-Eat-BE-main (1)/Balence-Eat-BE-main/app/dependencies.py
+++
@@ -1,9 +0,0 @@
-from app.database import SessionLocal
-
-
-def get_db():
-    db = SessionLocal()  # 세션 객체 생성
-    try:
-        yield db  # 세션 객체 반환
-    finally:
-        db.close()  # 요청이 끝나면 세션 종료
